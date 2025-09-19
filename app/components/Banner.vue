eb5103be809f39f64c0632d392b12bd316b31039
Mateen131 <Shahzada.Mateen@netsoltech.com>
2025-09-19 15:51:33 +0500

    banner

--- app/components/Banner.vue
+++ app/components/Banner.vue
@@ -1,33 +1,45 @@
 <template>
-  <section class="w-full bg-white border-b ml-[-40px]">
-    <div class="max-w-7xl mx-auto px-6">
-      <div class="grid grid-cols-1 md:grid-cols-2 gap-8 items-center py-12 md:py-16 ">
 
-        <div class="space-y-4 ">
-          <p class="text-sm tracking-wider text-gray-500 uppercase">Connect Sales</p>
+  <section class="w-full bg-white border-b mt-6 relative">
+    <div class="max-w-7xl mx-20 px-6">
 
-          <h1 class="text-3xl md:text-4xl font-qualy leading-tight">
-            Explore <span class="text-blue-600 ">NETSOL</span><span class="block md:inline"> Technologies</span>
-          </h1>
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-8 items-start py-12 md:py-16">
 
-          <p class="text-gray-600 max-w-lg">
-            With strategically located centers worldwide, we futureproof operations for financial institutions
-            globally.
+
+        <div class="space-y-4 self-start">
+
+          <p class="text-xs tracking-wider text-gray-500 pl-4 pt-8 font-bold">
+            Shaping Smarter Finance
           </p>
 
-          <NuxtLink to="/contact"
-            class="inline-block mt-4 px-6 py-3 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 transition duration-300">
-            Get in Touch
-          </NuxtLink>
+
+          <div class="text-[40px] font-bold font-qualy">
+            AI-powered ecosystems <br />
+            that make commerce
+            <span class="text-blue-600 font-qualy">transparent</span>
+          </div>
+
+
+          <p class="text-lg text-gray-600 max-w-lg">
+            We remove complexity from the asset lifecycle with a
+            composable platform that integrates effortlessly.
+          </p>
+
+
+          <div class="pt-10">
+          <div
+            class="inline-flex items-center mt-6 px-6 py-3 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 transition duration-300">
+            Get in touch
+          </div>
+          </div>
         </div>
 
-        <div class="flex justify-center md:justify-end mr-20">
 
-          <NuxtImg src="/images/Banner/broker.png" alt="NETSOL" />
+        <div class="flex justify-center md:justify-end mr-24">
+          <NuxtImg src="/images/Banner/broker.png" alt="NETSOL" width="400px" height="520px" />
         </div>
 
       </div>
     </div>
   </section>
-
 </template>
